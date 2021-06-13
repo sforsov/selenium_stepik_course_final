946371e7199fd190e8c27468520a11dd52c635e8
Serge <s.forsov@gmail.com>
2021-06-13 12:35:57 +0300

    add base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,8 +1,7 @@
 class BasePage():
-	url = "http://selenium1py.pythonanywhere.com/"
-	def __init__(self, browser, url):
-    self.browser = browser
-    self.url = url
+    def __init__(self, browser, url):
+        self.browser = browser
+        self.url = url
 
     def open(self):
-    self.browser.get(self.url)
+    	self.browser.get(self.url)
